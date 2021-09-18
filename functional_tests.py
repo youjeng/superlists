0e32bf9437d7e8798854dc8854aa2b2f5df12fab
mcheich <mcheich@gmail.com>
2021-09-18 13:52:33 -0400

    function test not check we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -16,20 +18,36 @@ class NewVisitorTest(unittest.TestCase):
         
         # She notices the page title and header mention To-Do lists
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
         
-
         # She is invited to enter a todo item straight away
+        input_box = self.browser.find_element_by_id('id-new-item')
+        self.assertEqual(
+            input_box.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
 
         # She types "Fill out paper work for ODNR"
+        input_box.send_keys('Fill out paper work for ODNR')
 
         # When she hits enter, the page updates, and not the page lists
         # "1: Fill out paper work for ODNR" as an item in a to-do list
+        input_box.send_keys('Fill out paper work for ODNR')
+        time.sleep(1)
+        
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Fill out paper work for ODNR' for row in rows)
+        )
 
         # There is still a text box inviting her to add another item.
         # She enters "order parts for bike shop"
+        self.fail('Finish the test!')
 
         # The page updates again, and shows both items on her list
+
 
         # Edith wonders if the site will remember her list
         # She see's that the site has generated a unique URL for her  -- there is 

3a48161511322661b1e427cb942d07a6866b2c59
mcheich <mcheich@gmail.com>
2021-09-29 06:31:30 -0400

    updated FT

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.firefox.webdriver import WebDriver
+import unittest
 import time
 
 MAX_WAIT = 10
@@ -74,7 +75,33 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox.send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table('1: Buy peacock feathers')
 
-        
+        # She notices that her list has a unique URL
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/lists/.+')
+    
+        # Now a new user, Francis, comes along to the site
+
+        ## We use a new browser session to make sure that no information
+        ## of Edith's is coming through from cookies
+        self.browser.quit()
+        self.browser.webdriver.Firefox()
+
+        # Francis visits the home page, there is no sign of Ediths lists
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('order parts', page_text)
+
+        # Francis starts a new list by entering a new item. He is less interesting than Edith
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy milk')
+
+        # Francis gets his own unique URL
+        fancis_list_url = self.browser.current_url
+        assertRegex(francis_list_url, '/lists/+')
+
 
         # Edith wonders if the site will remember her list
         # She see's that the site has generated a unique URL for her  -- there is 
